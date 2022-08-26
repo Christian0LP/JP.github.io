1751c72a137d75f3e777eebcc6df9f35ea33f868
Christian0LP <90888967+Christian0LP@users.noreply.github.com>
2022-08-26 10:51:24 -0500

    Add files via upload

---
+++ Other/nosotros.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>NOSOTROS</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="author" content="Juntosporelperu.com.pe">
+	<meta name="description" content="Juntos por el peru">
+
+	<!-- Favicon -->
+	<link rel="shortcut icon" href="../assets/Imagenesnew/cropped-logo_jp-icon-180x180.png">
+
+	<!-- Google Font -->
+	<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900%7CPlayfair+Display:400,400i,700,700i%7CRoboto:400,400i,500,700" rel="stylesheet">
+
+	<!-- Plugins CSS -->
+	<link rel="stylesheet" type="text/css" href="../assets/vendor/font-awesome/css/font-awesome.min.css" />
+	<link rel="stylesheet" type="text/css" href="../assets/vendor/themify-icons/css/themify-icons.css" />
+	<link rel="stylesheet" type="text/css" href="../assets/vendor/animate/animate.min.css" />
+	<link rel="stylesheet" type="text/css" href="../assets/vendor/fancybox/css/jquery.fancybox.min.css" />
+	<link rel="stylesheet" type="text/css" href="../assets/vendor/owlcarousel/css/owl.carousel.min.css" />
+	<link rel="stylesheet" type="text/css" href="../assets/vendor/swiper/css/swiper.min.css" />
+	<link rel="stylesheet" type="text/css" href="../assets/vendor/owlcarousel/css/owl.carousel.min.css" />
+
+	<!-- Theme CSS -->
+	<link rel="stylesheet" type="text/css" href="../assets/css/style.css" />
+	<link rel="stylesheet" type="text/css" href="../assets/css/color.css" />
+</head>
+<body>
+<header class="header-static navbar-sticky navbar-light ">
+		<!-- Logo Nav Start -->
+		<nav class="navbar navbar-expand-lg">
+			<div class="container">
+				<!-- Logo -->
+				<a class="navbar-brand" href="index.html">
+					<!-- SVG Logo Start -->
+					<img src="../assets/Imagenesnew/cropped-logo_jp.png" width="200"  >
+					<!-- SVG Logo End -->
+				</a>
+				<!-- Menu opener button -->
+				<button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+				  <span class="navbar-toggler-icon"> </span>
+			  </button>
+				<!-- Main Menu Start -->
+				<div class="collapse navbar-collapse " id="navbarCollapse">
+					<ul class="navbar-nav ml-auto">
+						<!-- Menu item 1 Demos-->
+						<li class="nav-item dropdown">
+							<a class="nav-link" href="../index.php"><b>INICIO</b></a>
+						</li>
+						<!-- Menu item 2 Blog-->
+						<li class="nav-item dropdown">
+							<a class="nav-link" href="../other/nosotros.php"><b>NOSOTROS</b></a>
+						</li>
+						<!-- Menu item 3 Pages-->
+						<li class="nav-item dropdown">
+							<a class="nav-link" href="../index.html"><b>COMUNICADOS</b></a>
+						</li>
+						<!-- Menu item 4 Portfolio-->
+						<li class="nav-item dropdown">
+							<a class="nav-link" href="../index.html"><b>NOTICIAS</b></a>
+						</li>
+						<!-- Menu item 5 Elements-->
+						<li class="nav-item dropdown">
+							<a class="nav-link" href="../index.html"><b>CONTACTAR</b></a>
+						</li>
+					</ul>
+				</div>
+				<!-- Main Menu End -->
+			</div>
+		</nav>
+	
+	</header>
+	<footer class="footer bg-light pt-1">
+		<div class="footer-content pb-3">
+			<div class="container">
+				<div class="row">
+					<!-- Footer widget 1 -->
+					<div class="col-md-2">
+						<div class="widget">
+							<a href="index.html" class="footer-logo">
+								<!-- footer SVG logo Start -->
+								<a class="navbar-brand text-center" href="index.html">
+									<!-- SVG Logo Start -->
+									<img src="../assets/Imagenesnew/cropped-logo_jp.png" width="500" height="300"  >
+									<!-- SVG Logo End -->
+								</a>
+								<!-- footer SVG logo End -->
+							</a>
+						</div>
+					</div>
+					<!-- Footer widget 2 -->
+					<div class="col-md-3 mt-30">
+						<div class="col-md-7 offset-lg-1 col-lg-5 mt-1 mt-md-0 mr-auto">
+							<h1 class="display-5 display-xl-5 font-weight-bold cd-headline clip clip-dark big-clip is-full-width">
+								<span class="cd-words-wrapper pt-0 text-dark">
+									<b class="is-visible">#JuntosPorElPerú</b>
+									<b class="text-center">#ElPartidoDelPueblo</b>
+								</span>
+							</h1>
+						</div>
+					</div>
+					<!-- Footer widget 3 -->
+					<div class="col-md-3 col-sm-6">
+						<div class="widget">
+							<h6></h6>
+							<ul class="nav flex-column primary-hover">
+							</ul>
+						</div>
+					</div>
+					<!-- Footer widget 4 -->
+					<!-- Footer widget 3 -->
+					<div class="col-md-4 col-sm-4 order-sm-3 pt-1 text-dark">
+						<div class="widget">
+							<h6><B>INFO</B></h6>
+							<ul class="nav flex-column">
+								<li class="nav-item"><b>Jr. Espinar 215 - Quillabamba</b></li>
+								<li class="nav-item"><b>Contacto@juntosporelperu.com.pe</b></li>
+								<ul class="social-icons si-colored-bg light">
+									<p>
+										<a href="https://bit.ly/3zWp2Wz" target="_blank"><img src="../assets/Imagenesnew/logoredes/facebook.png" alt="" width="28" height="28"></a>
+										<a href="https://bit.ly/3txod3Z" target="_blank"><img src="../assets/Imagenesnew/logoredes/instagram.png" width="28" height="28" /></a>
+										<a href="https://bit.ly/3nn7vU3" target="_blank"><img src="../assets/Imagenesnew/logoredes/twitter.png" width="28" height="28" /></a>
+										<a href="https://bit.ly/3zXk5gc" target="_blank"><img src="../assets/Imagenesnew/logoredes/whatsapp.png" width="28" height="28" /></a>
+									</p>
+								</ul>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<!-- Footer widget 4 -->
+			</div>
+		</div>
+		<div class="divider mt-3"></div>
+		<!--footer copyright -->
+		<footer class="footer bg-light py-3 text-dark">
+			<div class="footer-content">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-5 mx-auto text-center">
+							<p><b>Copyright 2022 &copy; <a Class="text-grad" href="#">Jhon V. & Gustavo Ch.</a>  Todos los derechos reservados</b></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+	</footer>
+	<!-- =======================
+	footer  -->
+
+    <div> <a href="#" class="back-top btn btn-grad"><i class="ti-angle-up"></i></a> </div>
+	
+</body>
+</html>
+
